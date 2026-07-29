befc2f0e0e1e6557b9785886e334d7372d7c2998
shriyarastogi2006-bit <shriyarastogi2006@gmail.com>
2026-07-29 15:09:43 +0530

    commit

---
+++ j1.js
@@ -0,0 +1,29 @@
+console.log("hello")
+let a=24
+let b=34
+let sum=a+b
+console.log("sum:",a+b)
+var name="shriya"
+console.log(name)
+const c="spain"
+console.log(c)
+console.log(typeof(c))
+student={
+    name:"shriya",
+    rollno:1130,
+    age:19,
+};
+console.log(typeof(student))
+const s={
+    name:"shriya",
+    age:"19"
+    
+};
+console.log(s)
+s.name="nitin"
+console.log(s)
+s.city="newyork"
+console.log(s)
+const f=["ibiza","vegas","santorini"];
+f.push("LA")
+console.log(f)
